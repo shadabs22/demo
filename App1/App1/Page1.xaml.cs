38f8fa5334e7f4cba8bf5b9db1d2f4e829459594
lenovo <lenovo@DESKTOP-KFVPLEJ>
2017-08-04 02:39:08 +0530

    Add project files.

---
+++ App1/App1/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender,EventArgs e)
+        {
+            MainLabel.Text = "Hello " + MainEntry.Text;
+                
+        }
+
+        private async void navigateToSecondPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2(MainEntry.Text));
+        }
+
+        
+    }
+}
---
+++ App1/App1/Page2.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string label)
+        {
+            InitializeComponent();
+            MainLabel.Text = label;
+            MainListView.ItemsSource = new List<Person> {
+                new Person {Name="aaa",Age="18" },
+                new Person {Name="bbb",Age="22" },
+                new Person {Name="cccc",Age="23" },
+                new Person {Name="ddd",Age="21" },
+                new Person {Name="eee",Age="25" },
+                new Person {Name="ffff",Age="20" },
+                new Person {Name="ggg",Age="19" },
+                new Person {Name="hhh",Age="18" },
+                new Person {Name="iiii",Age="40" },
+                new Person {Name="jjj",Age="55" },
+                new Person {Name="kkk",Age="56" },
+                new Person {Name="lll",Age="57" },
+                new Person {Name="mmm",Age="58" },
+                new Person {Name="nnn",Age="59" },
+                new Person {Name="oooo",Age="70" },
+            };
+        }
+    }
+}
